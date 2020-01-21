9d08e313dc10f86a3cd6ad0ef53106749750e353
Tyler Long <tyler4long@gmail.com>
2020-01-21 15:05:19 -0800

    enable and disable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
-axios.defaults.headers.common['X-Client-Key'] = process.env.appfigures_client_key
-axios.defaults.headers.common.Authorization = `Basic ${process.env.basic_authorization_key}`
+axios.defaults.headers.common['X-Client-Key'] = process.env.APPFIGURES_CLIENT_KEY
+axios.defaults.headers.common.Authorization = `Basic ${process.env.BASIC_AUTHORIZATION_KEY}`
 
 axios.get('https://api.appfigures.com/v2/reviews')
   .then(r => {
--- lambda.js
+++ lambda.js
@@ -2,6 +2,7 @@ const createApp = require('ringcentral-chatbot/dist/apps').default
 const { createAsyncProxy } = require('ringcentral-chatbot/dist/lambda')
 const serverlessHTTP = require('serverless-http')
 const axios = require('axios')
+const { Service } = require('ringcentral-chatbot/dist/models')
 
 const handle = async event => {
   const { type, text, group, bot } = event
@@ -9,9 +10,32 @@ const handle = async event => {
     if (text === 'ping') {
       await bot.sendMessage(group.id, { text: 'pong' })
     } else if (text === 'enable') {
-      await bot.sendMessage(group.id, { text: 'enabled!' })
+      const one = await Service.findOne({
+        where: {
+          name: 'RingCentral Apps Reviews',
+          groupId: group.id,
+          botId: bot.id
+        }
+      })
+      if (one !== null) {
+        await bot.sendMessage(group.id, { text: 'RingCentral Apps Reviews notification had been enabled for this team.' })
+        return
+      }
+      await Service.create({
+        name: 'RingCentral Apps Reviews',
+        groupId: group.id,
+        botId: bot.id
+      })
+      await bot.sendMessage(group.id, { text: 'RingCentral Apps Reviews notification has been enabled for this team.' })
     } else if (text === 'disable') {
-      await bot.sendMessage(group.id, { text: 'disabled!!' })
+      await Service.destroy({
+        where: {
+          name: 'RingCentral Apps Reviews',
+          groupId: group.id,
+          botId: bot.id
+        }
+      })
+      await bot.sendMessage(group.id, { text: 'RingCentral Apps Reviews notification has been disabled for this team.' })
     }
   }
 }
