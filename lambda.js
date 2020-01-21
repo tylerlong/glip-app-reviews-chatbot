626ad7edab2f5a998e71831eb81e37c40a20c71f
Tyler Long <tyler4long@gmail.com>
2020-01-21 14:35:14 -0800

    Going to implement enable and disable commands

--- lambda.js
+++ lambda.js
@@ -5,8 +5,14 @@ const axios = require('axios')
 
 const handle = async event => {
   const { type, text, group, bot } = event
-  if (type === 'Message4Bot' && text === 'ping') {
-    await bot.sendMessage(group.id, { text: 'pong' })
+  if (type === 'Message4Bot') {
+    if (text === 'ping') {
+      await bot.sendMessage(group.id, { text: 'pong' })
+    } else if (text === 'enable') {
+      await bot.sendMessage(group.id, { text: 'enabled!' })
+    } else if (text === 'disable') {
+      await bot.sendMessage(group.id, { text: 'disabled!!' })
+    }
   }
 }
 const app = createApp(handle)
