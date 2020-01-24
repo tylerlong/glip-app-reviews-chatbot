c81afd7f8200c2644071d0bb2f9a2cd865d3ace1
Tyler Long <tyler4long@gmail.com>
2020-01-24 15:03:11 -0800

    Update the test code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
+const moment = require('moment-timezone')
 
 axios.defaults.headers.common['X-Client-Key'] = process.env.APPFIGURES_CLIENT_KEY
 axios.defaults.headers.common.Authorization = `Basic ${process.env.BASIC_AUTHORIZATION_KEY}`
 
-axios.get('https://api.appfigures.com/v2/reviews')
-  .then(r => {
-    console.log(r.data)
-  }).catch(e => {
-    console.log(e)
-  })
+;(async () => {
+  const r = await axios.get('https://api.appfigures.com/v2/reviews')
+  const oneDayAgo = moment().add(-1, 'day').utc().format()
+  const newReviews = r.data.reviews.filter(review => moment(review.date).tz('EST').utc().format() > oneDayAgo)
+  console.log(newReviews)
+})()
